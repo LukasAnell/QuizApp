ef42a8fdfd091ea8b5dbefa79707b87e988d05cf
Lukas Anell <lukasmanell@gmail.com>
2023-09-20 22:55:34 -0700

    Added logic to check if button chosen is the correct answer. Created main game loop.

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonChoiceThree: Button
     lateinit var buttonChoiceFour: Button
     lateinit var textViewScore: TextView
+    var score = 0
 
     companion object {
         val TAG = "MainActivity"
@@ -28,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         wireWidgets()
         resizeButtonText(30.0F)
+        textViewQuestion.text = "Score: $score"
+        textViewQuestion.textSize = 10.0F
 
         // load questions from JSON (we will learn how to do this next class)
         val inputStream = resources.openRawResource(R.raw.questions)
@@ -46,29 +49,29 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "Q: ${newQuiz.questions[0].question}\nC:${newQuiz.questions[0].choices}\nA:${newQuiz.questions[0].answer}")
 
         // do the initial question & answer choices setup
-        var score = 0
-        for(i in 0..newQuiz.questions.size) {
-            var answer = newQuiz.questions[i].answer
+        for(i in 0..<newQuiz.questions.size) {
             textViewQuestion.text = newQuiz.questions[i].question
             buttonChoiceOne.text = newQuiz.questions[i].choices[0]
             buttonChoiceTwo.text = newQuiz.questions[i].choices[1]
             buttonChoiceThree.text = newQuiz.questions[i].choices[2]
             buttonChoiceFour.text = newQuiz.questions[i].choices[3]
+            buttonChoiceOne.setOnClickListener {
+                score += newQuiz.checkAnswer(i, 0)
+            }
+            buttonChoiceTwo.setOnClickListener {
+                score += newQuiz.checkAnswer(i, 1)
+            }
+            buttonChoiceThree.setOnClickListener {
+                score += newQuiz.checkAnswer(i, 2)
+            }
+            buttonChoiceFour.setOnClickListener {
+                score += newQuiz.checkAnswer(i, 3)
+            }
+            textViewScore.text = "Score: $score"
         }
+        Log.d(TAG, "Score: $score")
         // set listeners to react to user input
             // passing info to and from the Quiz object
-        buttonChoiceOne.setOnClickListener {
-
-        }
-        buttonChoiceTwo.setOnClickListener {
-
-        }
-        buttonChoiceThree.setOnClickListener {
-
-        }
-        buttonChoiceFour.setOnClickListener {
-
-        }
     }
 
     private fun resizeButtonText(size: Float) {
--- app/src/main/java/com/example/quizapp/Quiz.kt
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -2,4 +2,14 @@ package com.example.quizapp
 
 data class Quiz(
     val questions: List<Question>
-)
+) {
+    fun checkAnswer(questionNumber: Int, choice: Int): Int {
+        val question = questions[questionNumber]
+        val answer = question.answer
+        return if(question.choices[choice] == answer) {
+            1
+        } else {
+            0
+        }
+    }
+}
